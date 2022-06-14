fa54a373632d06d663c6a2a113b19ff505909c19
Jonathan Martinez <Jmartinez@Bestow.com>
2022-06-13 21:41:10 -0500

    added the full solution from gophercises, and finished my own solution as well.

--- main.go
+++ main.go
@@ -5,9 +5,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
-var answer string
 var correctAnswers int
 
 func main() {
@@ -21,11 +21,25 @@ func main() {
 	if err != nil {
 		fmt.Println("Error reading CSV file.")
 	}
+	timer := time.NewTimer(45 * time.Second)
+problemloop: //a label that labels the for loop.
 	for i, record := range records {
 		fmt.Printf("Problem #%d: %s = \n", i+1, record[0])
-		fmt.Scan(&answer)
-		if answer == record[1] {
-			correctAnswers += 1
+		answerCh := make(chan string) //creates an answer channel
+		go func() {
+			var answer string
+			fmt.Scan(&answer)
+			answerCh <- answer // when we get an answer, we are sending it to the answer channel
+		}()
+		select {
+		case <-timer.C: //timer.C blocks code until the timer is out and sends a message of completion on channel C.
+			// ^^^ if there is a message from timer.C channel, then go through this case.
+			fmt.Println("You ran out of time!")
+			break problemloop // breaks this specific loop.
+		case answer := <-answerCh: // if we get an answer from the answer channel
+			if answer == record[1] {
+				correctAnswers++
+			}
 		}
 	}
 	results := fmt.Sprintf("You scored %d out of %d!", correctAnswers, len(records))
--- quiz/quiz.go
+++ quiz/quiz.go
@@ -6,11 +6,13 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 // Starts the creator's implementation of the quiz
 func Quiz() {
 	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+	timeLimit := flag.Int("limit", 30, "the time limit for the quiz in seconds")
 	flag.Parse()
 
 	file, err := os.Open(*csvFilename)
@@ -24,18 +26,31 @@ func Quiz() {
 	}
 	problems := parseLines(lines)
 
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
 	correct := 0
 
+problemloop:
 	for i, p := range problems {
-		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-		if answer == p.a {
-			correct++
+		fmt.Printf("Problem #%d: %s = ", i+1, p.q)
+		answerCh := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Println()
+			break problemloop
+		case answer := <-answerCh:
+			if answer == p.a {
+				correct++
+			}
 		}
 	}
 
-	fmt.Printf("You scored %d out of %d. \n", correct, len(problems))
+	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
 }
 
 func parseLines(lines [][]string) []problem {
